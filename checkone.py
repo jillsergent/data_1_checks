099c2975f1ff9afbaff3b1b4b199f880023872f1
Jill <jill.sergent@gmail.com>
2022-10-09 23:11:26 -0400

    first check

---
+++ checkone.py
@@ -0,0 +1,10 @@
+print("Hello World!")
+
+hello_salem = ['itchita', 'copita', 'melaka', 'mystica']
+print(hello_salem[3])
+
+pamela_vorhees = {'first' : 'ki', 'second' : 'ma'}
+print(pamela_vorhees['first'])
+
+environment = ('Derry', 'Woodsboro', 'your dreams', 'Amity')
+print(environment[2])
